7f2e2839a320169fd530a986a87566d442861c31
Andrea Ivanova <84437502+AndreaI77@users.noreply.github.com>
2022-11-20 10:48:18 +0100

    primer commit con holaMundo

---
+++ a0_Hola_mundo/app/src/main/java/edu/andreaivanova/ej0101/MainActivity.kt
@@ -0,0 +1,46 @@
+package edu.andreaivanova.ej0101
+
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvSaludo: TextView
+    private lateinit var etSaludo: EditText
+    private lateinit var btnSaludar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // se inicializan las variables
+        tvSaludo = findViewById(R.id.tv_saludo)
+        etSaludo = findViewById(R.id.et_saludo)
+        btnSaludar = findViewById(R.id.btn_saludar)
+
+        btnSaludar.setOnClickListener { btn ->
+            // si no se inserta el texto
+            if (etSaludo.text.isBlank()) {
+                etSaludo.error = resources.getString(R.string.texto_error)
+                //crea un toast con el texto de error indicado
+                Toast.makeText(this, R.string.texto_error, Toast.LENGTH_SHORT).show()
+                btn.setBackgroundColor(Color.RED)
+            } else {
+                //si se inserta texto, aparece en el textView
+                etSaludo.error = null
+                tvSaludo.text = resources.getString(R.string.texto_saludo, etSaludo.text.trim())
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    btn.setBackgroundColor(this.getColor(R.color.purple_500))
+                } else {
+                    btn.setBackgroundColor(resources.getColor(R.color.purple_500))
+                }
+            }
+        }
+    }
+}
